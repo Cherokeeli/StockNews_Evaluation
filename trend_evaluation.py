d63cef5fbc230a066a8f636ccf754027a8ca73fe
QUAN Li <quanlime@gmail.com>
2017-04-11 03:41:56 +0800

    accuracy balance

--- trend_evaluation.py
+++ trend_evaluation.py
@@ -48,24 +48,30 @@ def dataPreloaded(path):
     authors = np.array([])
     for author in x_author:
         temp = convertToMatrix(author)
-        print('author %s hashing...' % author)
+        # print('author %s hashing...' % author)
         authors = np.append(authors, temp)
     authors = authors.reshape(len(dataset), 11025)
     train_y = np.column_stack((y_increase, y_decrease))
+    # print(train_y)
     train_x = np.array(dataset.iloc[:len(dataset),0:45])
     return train_x, train_y, authors
 # for v in y_set:
 #     batch = np.array([v, (v^1)]) # generate reverse label array
 #     # print(batch)
 #     train_y = np.vstack((train_y, batch))
-x_train, y_train, a_train = dataPreloaded('trend_train-author.csv')
-x_test, y_test, a_test = dataPreloaded('trend_test-author.csv')
+x_train, y_train, a_train = dataPreloaded('trend_train-author_nospace.csv')
+x_test, y_test, a_test = dataPreloaded('trend_test-author_nospace.csv')
+x_r_test, y_r_test, a_r_test = dataPreloaded('trend_test-author_allrise.csv')
+x_d_test, y_d_test, a_d_test = dataPreloaded('trend_test-author_alldrop.csv')
 
 # print(x_train.shape,y_train.shape,a_train.shape,x_test.shape,y_test.shape,a_test.shape)
 
 
 # print("shape is ",train_x)
 # Model parameter
+learning_rate = tf.placeholder(tf.float32, shape=[])
+W = tf.Variable(tf.zeros([2, 2]))
+b = tf.Variable(tf.zeros([2]))
 W1 = tf.Variable(tf.zeros([45, 2])) #layer1
 b1 = tf.Variable(tf.zeros([2]))
 
@@ -82,30 +88,39 @@ x2 = tf.placeholder(tf.float32, shape=(None, 11025))
 y1 = tf.nn.softmax(tf.matmul(x1, W1) + b1)
 # y2_1 = (tf.matmul(x2, W2_1) + b2_1)
 # y2_2 = (tf.matmul(y2_1, W2_1) + b2_1)
-y2 = tf.nn.softmax(tf.matmul(x2, W2_1) + b2_1)
+y2 = (tf.matmul(x2, W2_1) + b2_1)
 
 y_ = tf.placeholder(tf.float32, shape=(None,2))
-y = y1 + y2
+
+# y = tf.nn.sigmoid(tf.matmul(y1+y2,W) + b)
+y = y1
 # Loss
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y),axis=1))
 
 # Optimizer
-train_step = tf.train.GradientDescentOptimizer(0.05).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(learning_rate = learning_rate).minimize(cross_entropy)
 
 def next_batch(train, test, author, i, size):
     return train[i*size:(i+1)*size], test[i*size:(i+1)*size], author[i*size:(i+1)*size]
 sess = tf.InteractiveSession()
+# print('author shape is ',sess.run(add_author.shape()))
 tf.global_variables_initializer().run()
+
 # print(train_x.shape)
 for i in range(500):
     # print("x: ",np.array([1,2,3,4]).shape)
+    # lr = 0.1
     if (i%5==0):
         print('train: %d times, total: %d' % (i/5,100))
-    batch_x, batch_y, batch_a = next_batch(x_train, y_train,a_train,i,100)
+    batch_x, batch_y, batch_a = next_batch(x_train, y_train,a_train,i,10)
     # print(batch_y)
-    sess.run(train_step, feed_dict={x1: batch_x,x2:batch_a, y_: batch_y})
+    sess.run(train_step, feed_dict={x1: batch_x,x2:batch_a, y_: batch_y, learning_rate:0.09})
+
 
 correct_prediction = tf.equal(tf.argmax(y,1), tf.arg_max(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-print(sess.run(accuracy, feed_dict={x1: x_test,x2: a_test, y_: y_test}))
+print('total accuracy: ',sess.run(accuracy, feed_dict={x1: x_test,x2: a_test, y_: y_test}))
+print('rise accuracy: ',sess.run(accuracy, feed_dict={x1: x_r_test,x2: a_r_test, y_: y_r_test}))
+print('drop accuracy: ',sess.run(accuracy, feed_dict={x1: x_d_test,x2: a_d_test, y_: y_d_test}))
+# print(sess.run(y, feed_dict={x1: x_test,x2: a_test, y_: y_test}))
